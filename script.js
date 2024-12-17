c605045a785b1847d81897a6916d2252b6a5ce56
Relzarick <156683271+Relzarick@users.noreply.github.com>
2024-12-17 21:32:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -20,18 +20,21 @@ const checkValueP = () => {
 
 palindromeBtn.addEventListener("click", checkValueP);
 
+// telephone validator
 const validatorInput = document.getElementById("validator-input");
 const validatorCheck = document.getElementById("check-btn");
 const validatorClear = document.getElementById("clear-btn");
 const validatorResult = document.getElementById("validator-result");
 
 const checkValueV = (input) => {
-  const userInput = input;
+  validatorResult.textContent = "";
 
-  if (userInput === "") {
+  if (input === "") {
     alert("Please provide a phone number");
     return;
   }
+
+  // regex
   const countryCode = "^(1\\s?)?";
   const areaCode = "(\\([0-9]{3}\\)|[0-9]{3})";
   const spacesDashes = "[\\s\\-]?";
@@ -47,14 +50,87 @@ const checkValueV = (input) => {
   } US number: ${input}`;
   validatorInput.value = "";
 
-  console.log(userInput);
-};
-// idk what is forcefully clearing the result
-const clearValue = () => {
-  validatorResult.classList.add("hidden");
+  console.log(input);
 };
 
-validatorCheck.addEventListener("click", () => {
+// idk what is forcefully clearing the result (it was the form element)
+const clearValue = () => {
+  validatorResult.classList.add("hidden");
+  validatorResult.textContent = "";
+};
+
+validatorCheck.addEventListener("click", (e) => {
+  e.preventDefault();
   checkValueV(validatorInput.value);
 });
-validatorClear.addEventListener("click", clearValue);
+validatorClear.addEventListener("click", (e) => {
+  e.preventDefault();
+  clearValue();
+});
+
+// roman number conversion
+const conversionResult = document.getElementById("converter-result");
+const conversionForm = document.getElementById("form");
+
+const numToRoman = (num) => {
+  const romaNum = [
+    ["M", 1000],
+    ["CM", 900],
+    ["D", 500],
+    ["CD", 400],
+    ["C", 100],
+    ["XC", 90],
+    ["L", 50],
+    ["XL", 40],
+    ["X", 10],
+    ["IX", 9],
+    ["V", 5],
+    ["IV", 4],
+    ["I", 1],
+  ];
+  const results = [];
+
+  romaNum.forEach((arr) => {
+    while (num >= arr[1]) {
+      results.push(arr[0]);
+      num -= arr[1];
+    }
+  });
+  return results.join("");
+};
+
+const converToInt = () => {
+  const numStr = document.getElementById("conversion-number").value;
+  const int = parseInt(numStr);
+  return int;
+};
+
+const checker = (ui, i) => {
+  if (i.value === "") {
+    conversionResult.innerHTML = `<p>Please enter a valid number</p>`;
+  } else if (ui < 1) {
+    conversionResult.innerHTML = `<p>Please enter a number greater than or equal to 1<p>`;
+  } else if (ui > 3999) {
+    conversionResult.innerHTML = `<p>Please enter a number less than or equal to 3999</p>`;
+  } else {
+    return true;
+  }
+};
+
+const conversionRender = () => {
+  const input = document.getElementById("conversion-number");
+  const userInput = converToInt();
+
+  conversionResult.classList.remove("hidden");
+
+  if (checker(userInput, input)) {
+    conversionResult.innerText = numToRoman(userInput);
+  }
+  input.value = "";
+};
+
+conversionForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  conversionResult.classList.remove("hidden");
+  conversionRender();
+});
